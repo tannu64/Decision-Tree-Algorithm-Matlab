f1893f9319e72cd4f3f25c3a710064d4c8582bce
TannuWorks <115189555+tannu64@users.noreply.github.com>
2025-03-20 11:48:57 -0700

    Add files via upload

---
+++ code1.m
@@ -0,0 +1,4 @@
+% Load the Fisher Iris dataset
+load fisheriris;
+% The dataset includes 150 observations for iris flowers and 4 features: sepal length, sepal width, petal length, and petal width.
+% 'species' contains the class labels
